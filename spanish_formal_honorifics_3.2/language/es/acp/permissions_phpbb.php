0a7f2748da3e953a9f85bcd3bbaafcd54295ec54
phpBB España <thekuka@gmail.com>
2015-11-25 20:14:51 +0100

    Update permissions_phpbb.php

--- spanish_formal_honorifics_3.2/language/es/acp/permissions_phpbb.php
+++ spanish_formal_honorifics_3.2/language/es/acp/permissions_phpbb.php
@@ -120,6 +120,7 @@ $lang = array_merge($lang, array(
 	'ACL_F_POST'		=> 'Puede iniciar nuevos temas',
 	'ACL_F_STICKY'		=> 'Puede publicar notas',
 	'ACL_F_ANNOUNCE'	=> 'Puede publicar anuncios',
+	'ACL_F_ANNOUNCE_GLOBAL'	=> 'Puede publicar anuncios globales',
 	'ACL_F_REPLY'		=> 'Puede responder temas',
 	'ACL_F_EDIT'		=> 'Puede editar sus mensajes',
 	'ACL_F_DELETE'		=> 'Puede borrar permanentemente sus propios mensajes',
